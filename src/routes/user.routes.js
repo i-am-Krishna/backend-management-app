8687fc620bd5d31ce135c2c8e4e15f08bf1cbdc0
i-am-Krishna <kumarkrishn663@gmail.com>
2024-10-22 14:53:23 +0530

    Add vercel

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,12 +3,19 @@ const express = require('express');
 // Creating a router instance for user-related routes
 const userRouter = express.Router();
 // Importing the user controller that contains the business logic
-userRouter.get('/hello', userController.heelo);
 const userController = require('../controllers/user.controller.js');
 // Importing validation rules for user data
 const validation = require('../validators/user.validators.js');
 // Importing the authentication middleware to protect certain routes
 const { authentication } = require('../middlewares/authentication.js');
+
+userRouter.get('/hello', userController.heelo);
+
+    // Route for user login with validation rules applied
+    userRouter.post('/login', validation.loginValidationRules, userController.login);
+
+    // Route for user signup with validation rules applied
+    userRouter.post('/signup', validation.signupValidationRules, userController.signup); 
 
 // Route to get all users; protected by authentication middleware
 userRouter.get('/', authentication, userController.getAllUsers);
@@ -21,12 +28,6 @@ userRouter.patch('/:id', authentication, userController.editUserById);
 
 // Route for user logout; protected by authentication middleware
 userRouter.post('/logout', authentication, userController.logout);
-
-// Route for user login with validation rules applied
-userRouter.post('/login', validation.loginValidationRules, userController.login);
-
-// Route for user signup with validation rules applied
-userRouter.post('/signup', validation.signupValidationRules, userController.signup);
 
 
 // Exporting the user router for use in the main application
